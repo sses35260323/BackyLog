564f48e19879b6572b6579f0cc97f8ccc9110af4
log <sses35260323@gmail.com>
2024-07-20 19:51:17 +0800

    Initial commit

---
+++ test.js
@@ -0,0 +1,4 @@
+document.getElementById('showButton').addEventListener('click', function() {
+    var inputValue = document.getElementById('inputField').value;
+    alert('你输入的内容是: ' + inputValue);
+});
