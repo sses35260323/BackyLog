f8f895d962706ddb46d36c21f48bbc9b9554b652
log <sses35260323@gmail.com>
2024-07-21 03:14:04 +0800

    Initial commit

--- test.js
+++ test.js
@@ -1,4 +1,23 @@
-document.getElementById('showButton').addEventListener('click', function() {
-    var inputValue = document.getElementById('inputField').value;
-    alert('你输入的内容是: ' + inputValue);
-});
+function goTofrontpage() {
+    window.location.href = "https://sses35260323.github.io/backylog.github.io/test.html";
+}
+
+function aboutUs() {
+    window.location.href = "https://sses35260323.github.io/backylog.github.io/test.html";
+}
+
+function ContactUs() {
+    window.location.href = "https://sses35260323.github.io/backylog.github.io/test.html";
+}
+
+function Lession() {
+    window.location.href = "https://sses35260323.github.io/backylog.github.io/test.html";
+}
+
+function Portfolio() {
+    window.location.href = "https://sses35260323.github.io/backylog.github.io/test.html";
+}
+
+function login() {
+    window.location.href = "https://sses35260323.github.io/backylog.github.io/test.html";
+}
